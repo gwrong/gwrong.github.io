46eb97967f8108adc5a7d8be2de93637a97b62bd
gwrong <gwrong@gatech.edu>
2015-09-07 09:19:32 -0400

    Added smooth scrolling

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+// Scroll the page
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
